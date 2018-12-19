7f0eeb073e7aee839582ec026aa80ba373e5fa3d
Alessandro Fragnani <alefragnani@hotmail.com>
2018-12-19 19:52:14 -0200

    docs updated for 2.0.0

--- src/whats-new/PascalFormatterContentProvider.ts
+++ src/whats-new/PascalFormatterContentProvider.ts
@@ -10,12 +10,12 @@ export class WhatsNewPascalFormatterContentProvider implements ContentProvider {
     provideHeader(logoUrl: string): Header {
         return <Header>{logo: <Image> {src: logoUrl, height: 50, width: 50}, 
             message: `Make your <b>Pascal</b> source code look the way you want. It's not just a matter of <b>spaces vs tabs</b>,
-            but using a <b>standardized</b> source code make it a lot more <b>easier to read</b>.`};
+            but using a <b>standardized</b> source code make it <b>a lot easier to read</b>.`};
     }
 
     provideChangeLog(): ChangeLogItem[] {
         let changeLog: ChangeLogItem[] = [];
-        changeLog.push({kind: ChangeLogKind.NEW, message: "<b>Multi-root</b> support"});
+        changeLog.push({kind: ChangeLogKind.NEW, message: "<b>OmniPascal</b> extension support"});
         changeLog.push({kind: ChangeLogKind.CHANGED, message: `Extracted from <b>Pascal</b> extension 
             <a title=\"Open Pascal Extension\" href=\"https://github.com/alefragnani/vscode-language-pascal/\">
             Pascal Extension</a>)</b>`});
