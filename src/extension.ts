5dbfd59e138927a183149c05a4b1d72f8b1a8288
Alessandro Fragnani <alefragnani@hotmail.com>
2018-12-29 15:19:08 -0200

    Removes extension activation message (Closes #3)

--- src/extension.ts
+++ src/extension.ts
@@ -35,7 +35,7 @@ export function activate(context: vscode.ExtensionContext) {
 
     // Use the console to output diagnostic information (console.log) and errors (console.error)
     // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "vscode-pascal-formatter" is now active!');
+    // console.log('Congratulations, your extension "vscode-pascal-formatter" is now active!');
 
     let provider = new WhatsNewPascalFormatterContentProvider();
     let viewer = new WhatsNewManager(context).registerContentProvider("pascal-formatter", provider);
