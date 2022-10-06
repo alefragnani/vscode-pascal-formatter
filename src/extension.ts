56f2a068486bba85dc8aa4b72ae05425580b6c0f
Alessandro Fragnani <alefragnani@hotmail.com>
2022-10-05 21:28:18 -0300

    Bump whats-new for web support (and new asWebviewUri API) - Closes #20

--- src/extension.ts
+++ src/extension.ts
@@ -22,11 +22,11 @@ const documentSelector = [
 
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
-export function activate(context: vscode.ExtensionContext) {
+export async function activate(context: vscode.ExtensionContext) {
 
     Container.context = context;
 
-    registerWhatsNew();
+    await registerWhatsNew();
 
     vscode.commands.registerCommand('pascalFormatter.editFormatterParameters', () => {
 
